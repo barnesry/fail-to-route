c86d7050ab050beb1e2e4221bce42240a0c69c3e
Jared Dunbar <jrddunbr@gmail.com>
2018-09-30 13:18:57 -0700

    converted to python3, and fixed some small errors. Some conversions may not be necessary, but they are the correct types at the end

--- ted_fetch.py
+++ ted_fetch.py
@@ -9,6 +9,8 @@ from jnpr.junos import Device
 from lxml import etree
 import argparse, logging, getpass, sys
 
+import json
+import pprint
 
 def main(args):
 
@@ -16,19 +18,41 @@ def main(args):
     user = args.user
     # no password as we're relying on local ssh keys
 
+    ted = None
+    js = ""
+
     # setup our connection
-    dev=Device(host=host, user=user, password=password)
+    dev=Device(host=host, user=user)
     try:
         dev.open()
 
         # retrieve TED
         ted = dev.rpc.get_ted_database_information({'format': 'json'})
 
+
+        print("=== raw response ===")
         print(ted)
+        print()
+    except Exception as e:
+        print ("Failed to retreive TED: {}".format(e))
+    
+    # convert ted to valid json string
+    json_str = str(ted).replace("'",'"')
 
-    except:
-        print "Failed to retreive TED"
+    print("=== json_str ===")
+    print(json_str)
+    print()
 
+    # converts the json string to a dictionary object (load the json into the correct types of Python variables, recursively, effectively.
+    js = json.loads(json_str)
+
+    print("=== json in python dict ===")
+    print(js)
+    print()
+
+    # optional easy-to read recursive dict/list (json too?) printing
+    print("=== python dict pretty printing ===")
+    pprint.pprint(js)
 
 # execute only if called directly (not as a module)
 if __name__ == "__main__":
